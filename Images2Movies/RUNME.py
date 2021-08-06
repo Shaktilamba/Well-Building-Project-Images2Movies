447b2f5cab96bb059d570c2805f666f1a7821f2a
Samuel Huguet <samhuguet1@gmail.com>
2021-08-06 16:34:25 +0100

    First commit of the ipynb and py RUNME files

---
+++ Images2Movies/RUNME.py
@@ -0,0 +1,7 @@
+# Import the necessary packages.
+from Images2Movies_module import *
+
+# A function to allow the user to select the folder containing the subfolders of images.
+# Function input args: none. 
+# Function output 1: The path of the folder in question. 
+directory = folder_selection_dialog()
