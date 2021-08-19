bd9a94685e81bc1ec4fbb672c3bed9d778af46b3
Samuel Huguet <samhuguet1@gmail.com>
2021-08-19 19:03:45 +0100

    Updating everything so that tests work for newer code

--- Images2Movies/Images2Movies_module.py
+++ Images2Movies/Images2Movies_module.py
@@ -1,26 +1,28 @@
 from tkinter import *
 from tkinter import filedialog
 import os as os
-import cv2
+from shutil import copyfile
 import time as time 
 import math
+import ffmpeg 
+import subprocess
 
 # A function to allow the user to select the folder contianing the subfolders of images.
-# Function input arg 1: create_all_videos [bool] --> When 0, asks for the Well diirectory. When 1, asks for the directory containing the parents.
+# Function input arg 1: create_all_videos [bool] --> When 0, asks for the Well diirectory. When 1, asks for the directory containing the village folders.
 # Function input arg 2: test [bool] --> When 1, will change the gui title to that of the test gui.
 # Function output 1: The path of the folder selected by the user. 
 def folder_selection_dialog(create_all_videos = 0,
                             test = 0):
     root = Tk()
     if test:
-        root.title('Please select the "test" folder within the downloaded package.')
-        root.filename = filedialog.askdirectory(initialdir="/", title="Please select the 'test' folder within the downloaded package.")
+        root.title('Please select the "Well-Building-Project-Images2Movies\tests" folder within the downloaded package.')
+        root.filename = filedialog.askdirectory(initialdir="/", title="Please select the 'Well-Building-Project-Images2Movies\tests' folder within the downloaded package.")
     else:
         if create_all_videos == 0:
             root.title('Please select the well directory for which you want to make a video')
             root.filename = filedialog.askdirectory(initialdir="/", title="Please select the directory containing the subfolders of image data")
         elif create_all_videos == 1:
-            root.title('Please select the folder containing the parent directories')
+            root.title('Please select the folder containing the village directories')
             root.filename = filedialog.askdirectory(initialdir="/", title="Please select the directory containing the subfolders of image data")
         else:
             raise Exception("Value of create_all_videos must be 0 or 1, and cannot be int type.")
@@ -32,25 +34,59 @@ def folder_selection_dialog(create_all_videos = 0,
 # A function to create a list of image paths, such that these images can later be used to make a movie. 
 # Function input arg 1: selected_directory [string] --> The directory containing the subfolders of images. 
 # Function input arg 2: file_type [string] --> The file extension that you want to detect. 
-# Function output 1: image_paths [list] --> A list of the image paths, where each path is a string. 
-def list_image_paths(selected_directory,
-                     file_type = '.jpg'):
+# Function output 1: renamed_images_dir [string] --> A string of the path of the folder containing the image copies with ffmpeg compatible names.
+# Function output 1: txt_path [string] --> A string of the path of txt containing the image paths.
+def rename_images(selected_directory,
+                  file_type = '.JPG'):
+    
+    # Create a new folder to store the sequentially named images. 
+    renamed_images_dir = os.path.join(selected_directory, 'renamed_images')
+    if not os.path.exists(renamed_images_dir):
+        os.makedirs(renamed_images_dir)
     
     # First, list the subfolders in the well directory.
-    subfolders = [_ for _ in os.listdir(selected_directory) if '.' not in _]
-
-    # Iterate through the list of subfolders and extract the images contained within them. 
-    image_paths = []
-    for i in range(len(subfolders)):
-        
-        current_subfolder = subfolders[i]
-        subfolder_dir = os.path.join(selected_directory, current_subfolder, 'DCIM')
-        images_in_subfolder = [os.path.join(subfolder_dir, image) for image in os.listdir(subfolder_dir) if image.endswith(file_type)]
-        image_paths.append(images_in_subfolder)
-        
-    image_paths = [image for sublist in image_paths for image in sublist]
+    subfolders = [_ for _ in os.listdir(selected_directory) if ('.' not in _) and ('renamed_images' not in _)]
     
-    return image_paths
+    # Create our txt file to store file names. 
+    txt_path = os.path.join(selected_directory, "image_paths.txt")
+    
+    with open(txt_path, 'w') as f:
+                            
+        # Iterate through the list of subfolders and extract the images contained within them. 
+        image_number = 1 
+        for i in range(len(subfolders)):
+
+            # Build the path to the DCIM folder for this iteration.
+            current_subfolder = subfolders[i]
+            subfolder_dir = os.path.join(selected_directory, current_subfolder, 'DCIM')
+
+            # Detect the EK folders which contain the images. 
+            EK_folders = [_ for _ in os.listdir(subfolder_dir) if '.' not in _]
+
+            for u in range(len(EK_folders)):
+
+                # Build the path to the current EK folder. 
+                current_EK_folder = EK_folders[u]
+                EK_folder_dir = os.path.join(subfolder_dir, current_EK_folder)
+
+                # Collect the image paths from that EK folder. 
+                images_in_EK_folder = [os.path.join(EK_folder_dir, image) for image in os.listdir(EK_folder_dir) if image.endswith(file_type)]
+
+                # Iterate through each image, copy it to a single folder with ffmpeg-compatible names. 
+                for p in range(len(images_in_EK_folder)):
+
+                    # Copy the image to the new dir with the ffmpeg corrected name. 
+                    new_image_path = os.path.join(renamed_images_dir, f"{'{0:07}'.format(image_number)}{file_type}")
+                    copyfile(images_in_EK_folder[p], new_image_path)
+                    image_number += 1
+                    
+                    # Write our new file path to the txt.
+                    f.write(f"file '{new_image_path}'\n")
+                            
+    # Close our txt file.
+    f.close()
+                            
+    return renamed_images_dir, txt_path
 
 # A function to return the list of well folders, such that images with can later be used to make a movie. 
 # Function input arg 1: selected_directory [string] --> The directory containing the well folders of images. 
@@ -73,58 +109,50 @@ def list_well_paths(selected_directory):
     return well_paths
 
 # A function to take the list of image paths, load in said images, and convert them to a movie. 
-# Function input arg 1: selected_directory [string] --> The well directory, as previously selected. 
-# Function input arg 1: create_all_videos [bool] --> When 0, asks for the Well diirectory. When 1, asks for the directory containing the parents.
+# Function input arg 1: selected_directory [string] --> The well or village directory, as previously selected. 
+# Function input arg 1: create_all_videos [bool] --> When 0, creates individual videos from the well directory. When 1, considers every well directory and makes videos for all of them.
 # Function input arg 3: frame_rate [int] --> The desired frame rate. 
 # Function input arg 4: movie_extension [string] --> Your desired movie file extension. Tested for .avi and .mp4. 
-# Function input arg 5: movie_resolution [int] --> A percentage value Between 100 and 1. When 100, the original resolution is preserved. When at 50, the width and height will be halved.
+# Function input arg 4: bitrate [string] --> Bitrate at which video is exported.
 # Function output 1: The movie will be saved to 'selected_directory'. 
 def create_movie(selected_directory,
                  create_all_videos = 0,
-                 file_type = '.jpg',
-                 frame_rate = 1,
-                 movie_extension = '.avi',
-                 movie_resolution = 100):
+                 file_type = '.JPG',
+                 frame_rate = '3',
+                 movie_extension = '.mp4',
+                 bitrate = '5000k'):
     
     if create_all_videos == 0:
     
         # Construct the movie name. 
-        path_components = os.path.normpath(selected_folder).split(os.path.sep)
+        path_components = os.path.normpath(selected_directory).split(os.path.sep)
         n = len(path_components)
-        movie_name = (f"{path_components[n-2]}_{path_components[n-1]}_fps{frame_rate}{movie_extension}")
+        movie_name = (f"{path_components[n-2]}_{path_components[n-1]}_fps{frame_rate}_{bitrate}{movie_extension}")
 
         # Get the image paths. 
-        image_paths = list_image_paths(selected_directory,
-                                       file_type = file_type)
+        renamed_images_dir, txt_path = rename_images(selected_directory,
+                                                     file_type = file_type)
     
         # Change the current working directory to the well folder. 
         os.chdir(selected_directory)
 
-        # Create the video. 
-        frame = cv2.imread(image_paths[0])
-        height, width, layers = frame.shape
-        height = math.ceil((height/100)*movie_resolution)
-        width = math.ceil((width/100)*movie_resolution)
-        video = cv2.VideoWriter(movie_name, 0, frame_rate, (width,height))
-        
         # Start recording the processing duration.
         t = time.time() 
-        
-        for i in range(len(image_paths)):
-            t = time.time() # Start recording the processing duration.
 
-            # Print the progress as images are 'stiched' together over time. 
-            if i%100 == 0:
-                percentage = (i / len(image_paths))*100
-                print(f"Progress = {percentage}%")
-                
-            # Stitch the next image into our video.
-            video.write(cv2.imread(image_paths[i]))
-
-        cv2.destroyAllWindows()
-        video.release()
+        # Create the movie. 
+        (ffmpeg
+            .input('image_paths.txt', r=frame_rate, f='concat', safe='0')
+            .output(movie_name, vcodec='libx264', video_bitrate=bitrate)
+            .run())
+   
+        # Remove the image_paths.txt file. 
+        os.remove(txt_path)
+        shutil.rmtree(renamed_images_dir)
         
-        elapsed = time.time() - t # Calculate the processing duration.
+        # Calculate the processing duration.
+        elapsed = time.time() - t 
+        
+        # Print the completion statement.
         print(f"------------\nYour video, '{movie_name}' (FPS = {frame_rate}), has been saved to:\n\n{selected_directory}\nTime taken = {elapsed/60} min\n------------\n\n")
         
     if  create_all_videos == 1: 
@@ -137,38 +165,30 @@ def create_movie(selected_directory,
             # Construct the movie name. 
             path_components = os.path.normpath(well_paths[v]).split(os.path.sep)
             n = len(path_components)
-            movie_name = (f"{path_components[n-2]}_{path_components[n-1]}_fps{frame_rate}{movie_extension}")
+            movie_name = (f"{path_components[n-2]}_{path_components[n-1]}_fps{frame_rate}_{bitrate}{movie_extension}")
             
             # Get the image paths. 
-            image_paths = list_image_paths(well_paths[v],
-                                          file_type = file_type)
+            renamed_images_dir, txt_path = rename_images(selected_directory,
+                                                         file_type = file_type)
 
             # Change the current working directory to the well folder. 
             os.chdir(well_paths[v])
-
-            # Create the video. 
-            frame = cv2.imread(image_paths[0])
-            height, width, layers = frame.shape
-            height = math.ceil((height/100)*movie_resolution)
-            width = math.ceil((width/100)*movie_resolution)
-            video = cv2.VideoWriter(movie_name, 0, frame_rate, (width,height))
-
+            
             # Start recording the processing duration.
             t = time.time() 
-
-            for i in range(len(image_paths)):
-                t = time.time() # Start recording the processing duration.
-
-                # Print the progress as images are 'stiched' together over time. 
-                if i%100 == 0:
-                    percentage = (i / len(image_paths))*100
-                    print(f"Progress = {percentage}%")
-                
-                # Stitch the next image into our video.
-                video.write(cv2.imread(image_paths[i]))
-
-            cv2.destroyAllWindows()
-            video.release()
             
-            elapsed = time.time() - t # Calculate the processing duration.
+            # Create the movie.
+            (ffmpeg
+                .input('image_paths.txt', r=frame_rate, f='concat', safe='0')
+                .output(movie_name, vcodec='libx264', video_bitrate=bitrate)
+                .run())
+            
+            # Remove the image_paths.txt file. 
+            os.remove(txt_path)
+            shutil.rmtree(renamed_images_dir)
+
+            # Calculate the processing duration.
+            elapsed = time.time() - t 
+
+            # Print the completion statement.
             print(f"------------\nYour video, '{movie_name}' (FPS = {frame_rate}), has been saved to:\n\n{selected_directory}\nTime taken = {elapsed/60} min\n------------\n\n")
--- README.md
+++ README.md
@@ -6,20 +6,21 @@ E-mail: samhuguet1@gmail.com
 Date created: 11<sup>th</sup> August 2021
 
 ## Description: 
-- This code has been deisgned to consider several directories full of images, then 'stitch' them together to create a video. 
+- This code has been deisgned to consider several directories full of images, then 'stitch' those images together to create a video. 
 - This code was written for Dr. Shakti Lamba, to help process image data captured as part of the [well building project](http://shaktilamba.com/research-2/well-building-project/).
 - At this time of writing, Dr. Lamba is a researcher at The University of Exeter, Penryn campus. Her interest lies in "how people cooperate to solve public goods dilemmas", and in how this cooperation has evolved. 
-- The video output of this code is
+- I have included ```.ipynb``` and ```.py``` file, so that JupyterLab can be easily used alongside other IDEs. 
 
 ## Software requirements to run the code: 
 - You can run this code on your own computer for free, providing you download and install the following software:  
 (1) Anaconda: This is an immensely useful platform from which you can download the correct version of many languages with their respective packages. Anaconda also allows you to create specific environments for specific projects. You can manage packages and environments outside of Anaconda should you wish.  
 (2) Python 3.7. This can be installed through Anaconda.  
 (3) An IDE (Integrated development environment). This is the software which allows you to easily interact with the code. I would recommend JupyterLab, for which I have included many ```.ipynb``` files. JupyterLab can be installed through Anaconda.   
-(4) The packages required to create the correct environment. The package list will be stored in XXX.   
+(4) The packages required to create the correct environment. The package list will be stored [here](XXX).   
 
 ## The code expects your data to be organised in the following way: 
-- This code expects there to be multiple 'parent' folders (a.k.a. directories), each of which contains the images for an individual village. Within this parent directory, there needs to be a specific hierarchy of folders. 
+- This code expects there to be a single 'parent' folder (a.k.a. directories), which contains all the folders for individual villages. In my case, the parent directory was the (D:) drive, representing the hard-drive I was using.  
+- Within this parent directory, there needs to be a specific hierarchy of folders.   
 - This image gives a visual explanation of how to organise the data:
 
 <figure>
@@ -27,9 +28,9 @@ Date created: 11<sup>th</sup> August 2021
     <figcaption>Fig.1 - The heirarchy of folders needed for the code to work.</figcaption>
 </figure>  
   
-- It does not matter what the parent directories, wells or image folders are called, *providing* that they order correctly when you observe the folders when sorting by name. 
+- It does not matter what the parent directory, village folder, well folders, image folders or EK folders are called, *providing* that they order correctly when you observe the folders when sorting by name. E.g. If you're making a video for Well_X, then you'd want the folder ```100EK``` to come before folder ```101EK``` when sorting by name in windows explorer. 
 - The DCIM folders *must* be named 'DCIM'. 
-- Your images can be ```.jpg``` or ```.png```. You will simply need to chnage a input arg in the code (explained below). 
+- Your images can be ```.jpg``` or ```.png```. You will simply need to chnage a input arg in the code (explained below). This input arg will be case sensitive! 
 
 ## If you want, you can test that the code works before using it:
 It is often common practice to test that functions are working as exected. This is because software changes over time, and commands which might have done one thing, might be edited and to do another several years later. Thus, I have included additional 'test' code which checks that the primary functions are working as ecpected. Here's how to use the 'test' code:  
