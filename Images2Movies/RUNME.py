df69365ed4b7e0b83e6c8ecadfbff7abf2bfa05e
Samuel Huguet <samhuguet1@gmail.com>
2021-08-20 22:13:20 +0100

    Updating RUNME files with extra args

--- Images2Movies/RUNME.py
+++ Images2Movies/RUNME.py
@@ -2,7 +2,7 @@
 from Images2Movies_module import *
 
 # Variables
-create_all_videos = 0
+create_all_videos = 0 #[bool] --> When 0, asks for the Well diirectory. When 1, asks for the directory containing the parents.
 
 # A function to allow the user to select the folder contianing the subfolders of images.
 # Function input arg 1: create_all_videos [bool] --> When 0, asks for the Well diirectory. When 1, asks for the directory containing the parents.
@@ -12,15 +12,16 @@ folder_selection_dialog(create_all_videos = create_all_videos,
                         test = 0)
 
 # A function to take the list of image paths, load in said images, and convert them to a movie. 
-# Function input arg 1: selected_directory [string] --> The well directory, as previously selected. 
-# Function input arg 1: create_all_videos [bool] --> When 0, asks for the Well diirectory. When 1, asks for the directory containing the parents.
-# Function input arg 3: frame_rate [int] --> The desired frame rate. 
+# Function input arg 1: selected_directory [string] --> The well or village directory, as previously selected. 
+# Function input arg 1: create_all_videos [bool] --> When 0, creates individual videos from the well directory. When 1, considers every well directory and makes videos for all of them.
+# Function input arg 3: frame_rate [string] --> The desired frame rate. Pretend the value between the quotation marks is an [int].
 # Function input arg 4: movie_extension [string] --> Your desired movie file extension. Tested for .avi and .mp4. 
-# Function input arg 5: movie_resolution [int] --> A percentage value Between 100 and 1. When 100, the original resolution is preserved. When at 50, the width and height will be halved.
+# Function input arg 4: bitrate [string] --> Bitrate at which video is exported.
 # Function output 1: The movie will be saved to 'selected_directory'. 
 create_movie(selected_directory,
-             create_all_videos = create_all_videos,
-             file_type = '.jpg',
-             frame_rate = 1,
-             movie_extension = '.avi',
-             movie_resolution = 100)
+             create_all_videos = 0,
+             file_type = '.JPG',
+             frame_rate = '25',
+             movie_extension = '.mp4',
+             bitrate = '5000k',
+             video_width = 1920)
