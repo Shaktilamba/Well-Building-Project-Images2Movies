39ca08be46d2474ce716e0548b3cfa5d06337ea2
Samuel Huguet <samhuguet1@gmail.com>
2021-09-01 13:18:26 +0100

    Updating tests for new functions

--- tests/test_Images2Movies_module.py
+++ tests/test_Images2Movies_module.py
@@ -16,7 +16,7 @@ def test_folder_selection_dialog():
     print('Test 1 complete. folder_selection_dialog() is working as expected.\n')
 
 test_folder_selection_dialog()  
-    
+
 # Test the function 'rename_images()'.
 # Function input arg 1: test_directory [string] --> the test directory. 
 # Function output 1: When no errors are detected, a statement confirming this is printed.
@@ -24,8 +24,14 @@ def test_rename_images(current_directory):
     well_directory = os.path.join(current_directory, 'test_folder', 'test_village_1', 'Well_1')
     
     # Get the image paths. 
-    renamed_images_dir, txt_path = rename_images(well_directory,
-                                                 file_type = '.png')
+    image_list = make_image_list(well_directory,
+                                 file_type = '.png')
+    
+    renamed_images_dir = rename_images(well_directory,
+                                       image_list,
+                                       file_type = '.png')
+    
+    
     # Test 2. 
     assert os.path.isdir(renamed_images_dir) == True, "Test 1 has failed. test_rename_images did not create a new folder for the coppied (and renamed) images."
     print('Test 2 complete. rename_images made renamed_images_dir as expected.\n')
@@ -39,17 +45,8 @@ def test_rename_images(current_directory):
     print('Test 3 complete. rename_images copied the right number of images and gave then the right names.\n')
           
     # Test 4.
+    txt_path = renamed_images_dir.replace('renamed_images', 'image_paths.txt')
     assert os.path.exists(txt_path) == True, "Test 3 has failed. The txt file containing the image paths has not been made."
     print('Test 4 complete. rename_images made the txt file (image_paths).\n')
-    
-    # Test 5. 
-    # Read from the .txt file.
-    with open(txt_path, 'r') as f:
-        content = f.read()
-        txt_content = content.split(",")
-    f.close()
-    truth_txt_content = ["file 'C:\\Users\\Samuel Huguet\\OneDrive\\Documents\\GitHub\\Well-Building-Project-Images2Movies\\tests\\test_folder\\test_village_1\\Well_1\\renamed_images\\0000001.png'\nfile 'C:\\Users\\Samuel Huguet\\OneDrive\\Documents\\GitHub\\Well-Building-Project-Images2Movies\\tests\\test_folder\\test_village_1\\Well_1\\renamed_images\\0000002.png'\nfile 'C:\\Users\\Samuel Huguet\\OneDrive\\Documents\\GitHub\\Well-Building-Project-Images2Movies\\tests\\test_folder\\test_village_1\\Well_1\\renamed_images\\0000003.png'\nfile 'C:\\Users\\Samuel Huguet\\OneDrive\\Documents\\GitHub\\Well-Building-Project-Images2Movies\\tests\\test_folder\\test_village_1\\Well_1\\renamed_images\\0000004.png'\n"]
-    assert txt_content == truth_txt_content, "Test 5 has failed, the contents of the image_paths.txt is incorrect."
-    print('Test 5 complete. The contents of image_paths.txt is correct.\n')
     
 test_rename_images(current_directory)
